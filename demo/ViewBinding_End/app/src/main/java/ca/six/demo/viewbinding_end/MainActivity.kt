49075e038c00ac20c3fd7a38765091278e758a82
songzhw <songzhw2012@gmail.com>
2023-09-01 13:28:14 -0400

    new proj

---
+++ demo/ViewBinding_End/app/src/main/java/ca/six/demo/viewbinding_end/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.six.demo.viewbinding_end
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
